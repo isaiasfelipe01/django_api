75e371d3645f6227561261c9f8cfe5d43a96f814
Isaías Felipe Silva de Sousa <isaiasfelipe232@gmail.com>
2023-01-13 20:18:21 -0300

    Initial commit

---
+++ escola/views.py
@@ -0,0 +1,9 @@
+#from django.shortcuts import render
+from rest_framework import viewsets
+from escola.models import Aluno
+from escola.serializer import AlunoSerializer
+
+# Create your views here.
+class AlunoViewsSet(viewsets.ModelViewSet):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
